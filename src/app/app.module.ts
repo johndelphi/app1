7b19232ab9ceba5915426805db2bfc5371c481f0
John Michael <jmichael@coverys.com>
2022-02-18 23:07:01 -0600

    added  singout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SingoutComponent } from './login/singout/singout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    SingoutComponent
   ],
   imports: [
     BrowserModule
